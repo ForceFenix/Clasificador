70063571dc3d6bd7c7804d40a940e6d6ad613623
ForceFenix <55959592+ForceFenix@users.noreply.github.com>
2019-10-23 09:50:34 -0300

    comienzo de programa

---
+++ Creador.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+def diferenciador (lista):
+    textos = []
+    for i in lista:
+        if ('msg_' or '.txt' or '.TXT') in i:
+            textos.append(i)
+        else:
+            continue
+    return textos
+
+nombre = input("Escriba el nombre de la carpeta: ")
+ruta = ['LICENCIAS','SUMARIOS','TRASLADOS','CURSOS Y ESCALAFONES','VARIOS']
+os.chdir (r'C:/Users/SodaS/OneDrive/Escritorio/Pruebas para el trabajo')
+for i in ruta:
+    os.chdir (r'C:/Users/SodaS\OneDrive/Escritorio/Pruebas para el trabajo/'+ i)     #me situo
+    os.mkdir(nombre)                                                                 #creo
+    archivos = os.listdir()                                                          #convierto
+    a_mover = diferenciador (archivos)                                               #analizo
+    for a in a_mover:                                                                #muevo
+        shutil.move( a , nombre)
+
+
+print ('Carpetas creadas con el nombre de: ',nombre)
