73ab5d5f6fa898af7ddbf2e3ea712cf04bf6fa4b
ForceFenix <55959592+ForceFenix@users.noreply.github.com>
2019-10-31 23:11:09 -0300

    agregue un campo de pruebas

--- Testeo.py
+++ Testeo.py
@@ -1,5 +1,9 @@
+import io
+import os
 
-
-archivo_de_texto=open("C:/Users/SodaS/OneDrive/Escritorio/Pruebas/PruebaRadio/RADIOS/msg_416441","r")
+archivo_de_texto=open("C:/Users/SodaS/OneDrive/Escritorio/Pruebas/PruebaRadio/RADIOS/msg_416441.txt","r")
 texto= archivo_de_texto.read()
 archivo_de_texto.close();
+enter = texto.replace(" X ", "\n")
+print(enter)
+print(enter.split("\n",))
